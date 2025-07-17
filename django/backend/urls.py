9b5cdb99b8448683ef7719100047b5636cb8dae5
AJLI011 <aaronlaher@gmail.com>
2025-07-17 19:02:50 +0800

    modified to allow img upload

--- django/backend/urls.py
+++ django/backend/urls.py
@@ -16,14 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from accounts.views import reset_password
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('reset-password/<str:token>/', reset_password, name='reset_password'),  # needed for email reset
-    path('api/', include('accounts.urls')), 
 ]
+
+# Serve uploaded media files during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
