47ecd741a68c15b02b108bf6466ef16fbbba033f
AJLI011 <aaronlaher@gmail.com>
2025-07-17 12:54:20 +0800

    "copied updated code for customer view"

--- flutter/flutter_ui/lib/role_views/customer_view.dart
+++ flutter/flutter_ui/lib/role_views/customer_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+ 
 class CustomerView extends StatelessWidget {
   const CustomerView({super.key});
 
