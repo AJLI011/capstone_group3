ca43cf1b14efd225f7d42b595c3c9047c1c8a80a
AJLI011 <aaronlaher@gmail.com>
2025-07-30 19:20:30 +0800

    updated models file tailored to client business logic

--- django/accounts/models.py
+++ django/accounts/models.py
@@ -131,10 +131,9 @@ class Promo(models.Model):
     class Meta:
         db_table = 'promo_tbl'
 
-    inventory = models.OneToOneField('Inventory', on_delete=models.CASCADE, db_column='inventory_id')
-    get_free_quantity = models.PositiveIntegerField(default=0)
+    inventory_id = models.ForeignKey('Inventory', on_delete=models.CASCADE)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
-
+    
     def __str__(self):
-        return f"Promo for {self.inventory.medicine.name}"
+        return f"Promo for {self.inventory_id.medicine.name}"
