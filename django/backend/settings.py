aee93f92866c5271ee46abaf78f37537b1affdab
AJLI011 <aaronlaher@gmail.com>
2025-08-26 12:57:38 +0800

    updated password trying to fix

--- django/backend/settings.py
+++ django/backend/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'BlueWhitePharmacs$default',
         'USER': 'BlueWhitePharmacs',
-        'PASSWORD': 'admintest1',
+        'PASSWORD': 'group3pup',
         'HOST': 'bluewhitepharmacy.mysql.pythonanywhere-services.com',
     }
 }
