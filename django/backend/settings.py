70b4450a64723f16503d520055e8d8926d84b467
JhonElton <eltonjhonbantolino@gmail.com>
2025-09-07 09:46:06 +0800

    Save

--- django/backend/settings.py
+++ django/backend/settings.py
@@ -93,9 +93,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'capstone_db2',
+        'NAME': 'capstone_db',
         'USER': 'root',
-        'PASSWORD': 'admin123',
+        'PASSWORD': 'Fixthebreak_99',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
@@ -130,7 +130,7 @@ TIME_ZONE = 'Asia/Manila'
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 
