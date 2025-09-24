1c50511766e05316bef929a05e5acbd45e061fe6
jrmgn <jermagnenofre05@gmail.com>
2025-09-24 15:58:38 +0800

    setup

--- django/backend/settings.py
+++ django/backend/settings.py
@@ -94,9 +94,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'capstone_db7',
+        'NAME': 'capstone_db3',
         'USER': 'root',
-        'PASSWORD': 'admin123',
+        'PASSWORD': 'jermeyn12345',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
