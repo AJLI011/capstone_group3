cd675198300d13d2826a981b8ac9d2c608a03d7e
AJLI011 <aaronlaher@gmail.com>
2025-09-27 16:13:18 +0800

    changed database only

--- django/backend/settings.py
+++ django/backend/settings.py
@@ -94,9 +94,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'capstone_db3',
+        'NAME': 'capstone_db7',
         'USER': 'root',
-        'PASSWORD': 'jermeyn12345',
+        'PASSWORD': 'admin123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
