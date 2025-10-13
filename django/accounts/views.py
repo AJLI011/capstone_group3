67382a3620c951b09ad1aab396a732316861947d
jrmgn <jermagnenofre05@gmail.com>
2025-10-14 00:37:46 +0800

    updated files

--- django/accounts/views.py
+++ django/accounts/views.py
@@ -80,7 +80,7 @@ from rest_framework import generics, pagination
 #=============================
 
 from backend.firebase import send_fcm_notification
-from django.utils.timezone import now, localdate, get_current_timezone
+from django.utils.timezone import now, localdate, get_current_timezone #10/14/25
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -1474,7 +1474,7 @@ def remove_promo(request):
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
 
-
+#10/14/25
 def clean_expired_promos():
     today = localdate()
     
@@ -1493,6 +1493,7 @@ def clean_expired_promos():
         inventory_item.save()
         promo.delete()
 
+#10/14/25
 #---------------10/12/2025--------elton
 @api_view(['GET'])
 def promos_ending_soon(request):
@@ -1591,7 +1592,7 @@ def get_item_by_barcode(request, barcode):
     
     return Response(serializer.data, status=status.HTTP_200_OK)
 
-
+#10/14/25
 #===============9/13/2025=============================
 #----------Customer Side Mainview----------------
 
@@ -2966,7 +2967,7 @@ def finalize_online_order(request, orderId):
 
 
 
-  
+#10/14/25
 #--------------------------10-13-25----------------------------------------      
 #------instore sales transaction views----------------
 class InStoreSalesTransactionView(generics.ListAPIView):
