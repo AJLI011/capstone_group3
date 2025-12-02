bc24c7dbcec5fa43f08eb95e0de946a8cac86b50
AJLI011 <aaronlaher@gmail.com>
2025-12-03 00:38:40 +0800

    fixed url for meds view

--- flutter/flutter_ui/lib/role_views/customer_features/medicine_view.dart
+++ flutter/flutter_ui/lib/role_views/customer_features/medicine_view.dart
@@ -7,7 +7,7 @@ import 'medicine_detail_page.dart';
 // API base URL constant for consistent API calls.
 const String API_BASE = String.fromEnvironment(
   'API_BASE',
-  defaultValue: 'http://bluewhiteph.pythonanywhere.com:8000/',
+  defaultValue: 'http://bluewhiteph.pythonanywhere.com/',
 );
 
 class Medicine {
